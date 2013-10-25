17f9a85d261a1118cf2c489e3e4ac72c3fae1723
Anas Azeem <anas@socialcrawler.in>
2013-10-25 12:51:13 +0530

    Added Project

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\Users\Android\test\platforms\android\gen\com\test\test\R.java \
+ : C:\Users\Android\test\platforms\android\res\drawable\icon.png \
+C:\Users\Android\test\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Android\test\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Android\test\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Android\test\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Android\test\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Android\test\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Android\test\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Android\test\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\Android\test\platforms\android\res\layout\main.xml \
+C:\Users\Android\test\platforms\android\res\values\strings.xml \
+C:\Users\Android\test\platforms\android\res\xml\config.xml \
+C:\Users\Android\test\platforms\android\bin\AndroidManifest.xml \
